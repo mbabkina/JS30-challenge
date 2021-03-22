1c5bd51d0f49c3e16ebc2398b3f5b3d6e7a19888
maryna.babkina <babckina@gmail.com>
2021-03-22 20:07:48 +0200

    flex

---
+++ 05-flex/index.js
@@ -0,0 +1,11 @@
+const panels = document.querySelectorAll('.panel')
+
+function toggleOpen() {
+  this.classList.toggle('open')
+}
+
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) this.classList.toggle('open-active')
+}
+panels.forEach((p) => p.addEventListener('click', toggleOpen))
+panels.forEach((p) => p.addEventListener('transitionend', toggleActive))
